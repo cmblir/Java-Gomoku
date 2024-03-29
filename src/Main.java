2a42ee16325c7396d01e4ee9ee0ac293ff89b37b
cmblir <sodlalwl13@gmail.com>
2024-03-29 23:10:36 +0900

    fix .java : add Main concave game test

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,49 @@
+import main.board.BasicBoard;
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
+        System.out.println("오목 게임을 시작합니다.");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("원하는 오목판의 크기를 입력하세요.");
+        BasicBoard board = new BasicBoard(scanner.nextInt());
+        scanner.nextLine();
 
+        System.out.println("흑돌부터 시작합니다. 돌을 놔둘 좌표를 입력하세요.");
+        System.out.println("흑돌은 'O'이며, 입력 예시는 5,3 과 같이 입니다.");
+        int count = 0;
+
+        while (true) {
+            char stone;
+            if (count % 2 == 0) {
+                System.out.println("백돌 차례입니다. 좌표를 입력하세요.");
+                stone = 'X';
+            } else {
+                System.out.println("흑돌 차례입니다. 좌표를 입력하세요.");
+                stone = 'O';
+            }
+            String[] temp = scanner.nextLine().split(",");
+            int x = Integer.parseInt(temp[0]);
+            int y = Integer.parseInt(temp[1]);
+
+            if (isValidMove(board, x, y)) {
+                placeStone(board, x, y, stone);
+            } else {
+                System.out.println("유효하지 않은 위치입니다.");
+            }
+
+            board.displayBoard();
+            count++;
+        }
+    }
+
+    private static boolean isValidMove(BasicBoard board, int x, int y) {
+        char[][] gameBoard = board.getBoard();
+        int size = board.getSize();
+        return x >= 0 && x < size && y >= 0 && y < size && gameBoard[x][y] == '-';
+    }
+
+    private static void placeStone(BasicBoard board, int x, int y, char stone) {
+        char[][] gameBoard = board.getBoard();
+        gameBoard[x][y] = stone;
     }
 }
